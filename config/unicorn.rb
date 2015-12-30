700bd634e951ee2badfa6394e0aab3f1e7a37d4e
ChrisZoc <dona@stackbuilders.com>
2015-12-30 10:10:24 -0500

    The project is configurated

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,14 +1,19 @@
 # http://michaelvanrooijen.com/articles/2011/06/01-more-concurrency-on-a-single-heroku-dyno-with-the-new-celadon-cedar-stack/
+APP_PATH = "/var/www/apps/errbit/current"
+working_directory APP_PATH
+
+stderr_path APP_PATH + "/log/unicorn.stderr.log"
+stdout_path APP_PATH + "/log/unicorn.stderr.log"
+
+pid APP_PATH + "/tmp/pids/unicorn.pid"
 
 worker_processes 3 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
 preload_app true
-listen ENV['PORT'] || 8080
-pid ENV['UNICORN_PID'] if ENV['UNICORN_PID']
 
 # Taken from github: https://github.com/blog/517-unicorn
 # Though everyone uses pretty miuch the same code
-before_fork do |server, _worker|
+before_fork do |server, worker|
   ##
   # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
   # immediately start loading up a new version of itself (loaded with a new
@@ -19,13 +24,13 @@ before_fork do |server, _worker|
   # we send it a QUIT.
   #
   # Using this method we get 0 downtime deploys.
-
+ 
   old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exist?(old_pid) && server.pid != old_pid
+  if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
-      warn "Unicorn: master process already killed, no problem"
+      # someone else did our job for us
     end
   end
 end
